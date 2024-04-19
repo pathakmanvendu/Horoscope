95ed56d4bd30c44949e79b260168daf19fa3ab2e
Manvendu Pathak <manvendupathak2000@gmail.com>
2024-04-19 20:09:57 +0530

    Initial Commit

---
+++ Horoscope/HoroscopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoroscopeApp.swift
+//  Horoscope
+//
+//  Created by manvendu pathak  on 19/04/24.
+//
+
+import SwiftUI
+
+@main
+struct HoroscopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
